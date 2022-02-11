952c643ec958ea41875a6b09227cd14a933d62fe
Salvador aleguas <42396136+saleguas@users.noreply.github.com>
2022-02-10 21:02:51 -0500

    updates
    lol

--- src/core/equation_solver.py
+++ src/core/equation_solver.py
@@ -1,14 +1,21 @@
 from sympy import *
 
+
 def preprocess_equation(eq):
-    return "Eq(" + eq.replace("=", ",") + ")"
+    for i in range(1, len(eq)):
+        # check if there is a number right before a variable and replace it with a symbol
+        if eq[i].isalpha() and eq[i-1].isdigit():
+            # add an asterik between
+            eq = eq[:i] + "*" + eq[i:]
+
+    return sympify("Eq(" + eq.replace("=", ",") + ")")
 
 def solve_equation(eq):
     try:
         if "=" in eq:
             valid_eq = preprocess_equation(eq)
             print(valid_eq)
-            return sympify(solve(valid_eq))
+            return solveset(valid_eq)
         else:
             return sympify(eq)
     except:
