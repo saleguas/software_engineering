f177dde5de0130c06b5248a7c077ec49d96eccd9
Andrew <80601252+drewva@users.noreply.github.com>
2022-02-16 15:52:18 -0500

    add new option to UI
    changed a file name & a directory name
    
    also added a feature to the UI in which the user can simplify the explanation

--- src/UI/main.js
+++ src/UI/main.js
@@ -4,14 +4,28 @@ let input = document.getElementById("input");
 let outputHeader = document.getElementById("output header");
 let output = document.getElementById("output");
 let resetBtn = document.getElementById("reset");
+let length = document.getElementById("solution length");
+
+let fullSolution = "full solution goes here";
+let simpleSolution = "simple solution goes here";
 
 if (dataIndex != -1 && filepath.length > dataIndex + 10) {
 	//if there is an equation specified...
 	outputHeader.innerHTML = "Solution";
-	output.innerHTML = "solution goes here";
+	output.innerHTML = fullSolution;
+	//length.elements["solution length"].value = "full";
 }
 
 resetBtn.onclick = function() {
 	input.elements["equation"].value = "";
 	input.submit();
+}
+
+length.onchange = function() {
+	if (dataIndex != -1 && filepath.length > dataIndex + 10) {
+		if (length.elements["solution length"].value == "full")
+			output.innerHTML = fullSolution;
+		else
+			output.innerHTML = simpleSolution;
+	}
 }
