c144769d0e380ec3b104a826281325ff729ef998
adamsnev1 <adamsnev1@gmail.com>
2022-03-27 21:18:10 -0400

    updates 3
    yes

---
+++ src/custom_equation_solver/StaticFunctions.py
@@ -0,0 +1,30 @@
+from Multiply import Multiply
+from Add import Add
+
+
+def distribute(function):
+    if isinstance(function, Multiply):
+        for factor in function.factors:
+            if isinstance(factor, Add):
+                other_factors = [f for f in function.factors if f != factor]
+                new_addends = []
+                for addend in factor.addends:
+                    new_factors = other_factors.copy()
+                    new_factors.append(addend)
+                    new_multiply = Multiply(new_factors)
+                    new_addends.append(new_multiply)
+                return distribute(Add(new_addends))
+    elif isinstance(function, Add):
+        for i in range(len(function.addends)):
+            function.addends[i] = distribute(function.addends[i])
+    return function
+
+
+def remove_nesting(function):
+    if isinstance(function, Multiply):
+        if len(function.factors) == 1:
+            return function.factors[0]
+    if isinstance(function, Add):
+        if len(function.addends) == 1:
+            return function.addends[0]
+    return function
