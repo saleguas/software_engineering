e5e2e2597f9b9f6befdca8c91ec9ef113a536b31
RelativelyBlank <jeffreyma1008@gmail.com>
2022-04-13 11:10:29 -0400

    finished tests

--- src/tests/test_multiply.py
+++ src/tests/test_multiply.py
@@ -1,46 +1,44 @@
 import sys, os
-
-from custom_equation_solver.Constant import Constant
-
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "core"))
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "tests"))
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "custom_equation_solver"))
 
 from Multiply import Multiply
+from Constant import Constant
+from SimplePower import SimplePower
+from Add import Add
 import pytest
 
+
 @pytest.mark.parametrize(
-    ('input1','expected1'),
+    ('input1','input1_1','expected1'),
     (
-        pytest.param(Multiply([Constant(5),Constant(2)]),None,id='test x+1'),
+        pytest.param(SimplePower('x',2),SimplePower('x',3),5,id='x^2*x^3'),
+        pytest.param(SimplePower('x',1),SimplePower('x',1),2,id='x*x'),
+        pytest.param(SimplePower('x',3),SimplePower('x',4),7,id='x^3*x^4'),
+        pytest.param(SimplePower('x',20),SimplePower('x',20),40,id='x^20*x^20'),
+        pytest.param(SimplePower('x',9),SimplePower('x',3),12,id='x^9*x^3'),
+
     )
 )
-def test_multiply_combine_powers(input1,expected1):
-    pass
+def test_multiply_combine_powers(input1,input1_1,expected1):
+    function = Multiply([input1,input1_1])
+    function.combine_powers()
+    assert isinstance(function,Multiply)
+    assert len(function.factors) == 1
+    assert isinstance(function.factors[0],SimplePower)
+    assert function.factors[0].base == 'x'
+    assert function.factors[0].power == expected1
 
 @pytest.mark.parametrize(
     ('input2','expected2'),
     (
-        pytest.param(Multiply([Constant(5),Constant(2)]),None,id='test x+1'),
+        pytest.param(Multiply([Constant(5)]),True,id='test 5'),
+        pytest.param(Multiply([SimplePower('x',1),Constant(3)]),True,id='test 3x'),
+        pytest.param(Multiply([SimplePower('x',1)]),True,id='test x'),
+        pytest.param(Multiply([SimplePower('x',2)]),False,id='test x^2'),
+        pytest.param(Multiply([Constant(3),SimplePower('x',2)]),False,id='test 3x^2'),
     )
 )
-def test_simplify(input2,expected2):
-    pass
-
-@pytest.mark.parametrize(
-    ('input3','expected3'),
-    (
-        pytest.param(Multiply([Constant(5),Constant(2)]),None,id='test x+1'),
-    )
-)
-def test_is_linear(input3,expected3):
-    pass
-
-@pytest.mark.parametrize(
-    ('input4','expected4'),
-    (
-        pytest.param(Multiply([Constant(5),Constant(2)]),None,id='test x+1'),
-    )
-)
-def test_to_string(input4,expected4):
-    pass
+def test_is_linear(input2,expected2):
+    assert input2.is_linear() == expected2
--- src/tests/test_static_func.py
+++ src/tests/test_static_func.py
@@ -22,18 +22,95 @@ def test_distribute():
 
 def test_remove_nesting():
     pass
+def test_standardize_linear_format_1():
+    function = Add([SimplePower('x', Constant(1)), Constant(1)])
+    function = standardize_linear_format(function)
+    assert isinstance(function, Add)
+    for addend in function.addends:
+        if isinstance(addend, Multiply):
+            assert len(addend.factors) == 2
+            assert (isinstance(addend.factors[0], Constant) and isinstance(addend.factors[0], SimplePower)) or \
+                   (isinstance(addend.factors[1], Constant) and isinstance(addend.factors[0], SimplePower))
+            if isinstance(addend.factors[0], Constant):
+                assert addend.factors[0] == 1 and addend.factors[1].base == 'x' and addend.factors[1].power == 1
+            else:
+                assert addend.factors[1] == 1 and addend.factors[0].base == 'x' and addend.factors[0].power == 1
+        elif isinstance(addend, Constant):
+            assert addend == 1
+        else:
+            assert False
 
-@pytest.mark.parametrize(
-    ('input1','expected1'),
-    (
-        pytest.param(Constant(1),None,id='test x+1'),
-        pytest.param(2,None,id='test x+2'),
-        pytest.param(20,None,id='test x+20'),
-    )
-)
-def test_standardize_linear_format(input1,expected1):
-    pass
+def test_standardize_linear_format_2():
+    function = Add([Constant(0)])
+    function = standardize_linear_format(function)
+    assert isinstance(function, Add)
+    for addend in function.addends:
+        if isinstance(addend, Multiply):
+            assert len(addend.factors) == 2
+            assert (isinstance(addend.factors[0], Constant) and isinstance(addend.factors[0], SimplePower)) or \
+                   (isinstance(addend.factors[1], Constant) and isinstance(addend.factors[0], SimplePower))
+            if isinstance(addend.factors[0], Constant):
+                assert addend.factors[0] == 1 and addend.factors[1].base == 'x' and addend.factors[1].power == 1
+            else:
+                assert addend.factors[1] == 1 and addend.factors[0].base == 'x' and addend.factors[0].power == 1
+        elif isinstance(addend, Constant):
+            assert addend == 0
+        else:
+            assert False
 
+def test_standardize_linear_format_3():
+    function = Add([SimplePower('x',Constant(1))])
+    function = standardize_linear_format(function)
+    assert isinstance(function, Add)
+    for addend in function.addends:
+        if isinstance(addend, Multiply):
+            assert len(addend.factors) == 2
+            assert (isinstance(addend.factors[0], Constant) and isinstance(addend.factors[0], SimplePower)) or \
+                   (isinstance(addend.factors[1], Constant) and isinstance(addend.factors[0], SimplePower))
+            if isinstance(addend.factors[0], Constant):
+                assert addend.factors[0] == 1 and addend.factors[1].base == 'x' and addend.factors[1].power == 1
+            else:
+                assert addend.factors[1] == 1 and addend.factors[0].base == 'x' and addend.factors[0].power == 1
+        elif isinstance(addend, Constant):
+            assert addend == 1
+        else:
+            assert False
+
+def test_standardize_linear_format_4():
+    function = Add([SimplePower('x',Constant(1)),Constant(4)])
+    function = standardize_linear_format(function)
+    assert isinstance(function, Add)
+    for addend in function.addends:
+        if isinstance(addend, Multiply):
+            assert len(addend.factors) == 2
+            assert (isinstance(addend.factors[0], Constant) and isinstance(addend.factors[0], SimplePower)) or \
+                   (isinstance(addend.factors[1], Constant) and isinstance(addend.factors[0], SimplePower))
+            if isinstance(addend.factors[0], Constant):
+                assert addend.factors[0] == 1 and addend.factors[1].base == 'x' and addend.factors[1].power == 1
+            else:
+                assert addend.factors[1] == 1 and addend.factors[0].base == 'x' and addend.factors[0].power == 1
+        elif isinstance(addend, Constant):
+            assert addend == 4
+        else:
+            assert False
+
+def test_standardize_linear_format_5():
+    function = Add([Multiply([SimplePower('x',Constant(1)),2]),Constant(3)])
+    function = standardize_linear_format(function)
+    assert isinstance(function, Add)
+    for addend in function.addends:
+        if isinstance(addend, Multiply):
+            assert len(addend.factors) == 2
+            assert (isinstance(addend.factors[0], Constant) and isinstance(addend.factors[0], SimplePower)) or \
+                   (isinstance(addend.factors[1], Constant) and isinstance(addend.factors[0], SimplePower))
+            if isinstance(addend.factors[0], Constant):
+                assert addend.factors[0] == 1 and addend.factors[1].base == 'x' and addend.factors[1].power == 1
+            else:
+                assert addend.factors[1] == 1 and addend.factors[0].base == 'x' and addend.factors[0].power == 1
+        elif isinstance(addend, Constant):
+            assert addend == 3
+        else:
+            assert False
 
 @pytest.mark.parametrize(
     ('input2','expected2'),
